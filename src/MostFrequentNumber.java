4b685343a96aba2b4c05220d32ebf8ac189ab9f5
Guest <D1397009@o365.fcu.edu.tw>
2025-04-08 12:09:38 +0800

    儲存

--- src/MostFrequentNumber.java
+++ src/MostFrequentNumber.java
@@ -4,18 +4,17 @@ public class MostFrequentNumber {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int[] count = new int[10]; // 用來記錄每個數字出現的次數
-
-        System.out.println("請輸入 10 個 0-9 的數字：");
         for (int i = 0; i < 10; i++) {
             int num = scanner.nextInt();
             if (num >= 0 && num <= 9) {
                 count[num]++; // 累加該數字的出現次數
-            } else {
-                System.out.println("輸入的數字不在 0-9 範圍內！");
-                i--; // 如果輸入錯誤，重複此輪輸入
             }
         }
-
+        // 檢查是否有超過 10 個數字輸入
+        if (scanner.hasNextInt()) {
+            System.out.println("Out of range");
+            return; // 如果多於 10 個數字輸入，停止程式
+        }
         int maxCount = 0;
         int mostFrequentNum = -1;
 
@@ -26,6 +25,6 @@ public class MostFrequentNumber {
             }
         }
 
-        System.out.println("出現次數最多的數字是：" + mostFrequentNum + "，次數為：" + maxCount);
+        System.out.println( mostFrequentNum + "," + maxCount);
     }
 }
